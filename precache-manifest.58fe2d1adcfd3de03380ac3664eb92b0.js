2b8d87d085c34dcb91ae38e028db6948a4247b1a
Dmitriy <dmitriy.belyi88@gmail.com>
2020-06-29 01:06:55 +0300

    Updates

---
+++ precache-manifest.58fe2d1adcfd3de03380ac3664eb92b0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "447a96d9a7da95a9e2900bd8169044a1",
+    "url": "/goit-react-hw-01-components-/index.html"
+  },
+  {
+    "revision": "2c85514326f4e0fcf2b3",
+    "url": "/goit-react-hw-01-components-/static/css/main.3969c930.chunk.css"
+  },
+  {
+    "revision": "8d9dc58c1a4c85358da3",
+    "url": "/goit-react-hw-01-components-/static/js/2.3cf5bb85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components-/static/js/2.3cf5bb85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c85514326f4e0fcf2b3",
+    "url": "/goit-react-hw-01-components-/static/js/main.2411b5ff.chunk.js"
+  },
+  {
+    "revision": "c5a32da4795651ca2302",
+    "url": "/goit-react-hw-01-components-/static/js/runtime-main.ab0961f6.js"
+  }
+]);
